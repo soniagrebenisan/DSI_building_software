1b52b18fdc51528db039a14e965d6f0a1a0be55f
soniagrebenisan <sonia.grebenisan@gmail.com>
2024-02-12 17:14:51 -0500

    tried my best at config files

--- python_assignment_2_as_script.py
+++ python_assignment_2_as_script.py
@@ -3,28 +3,44 @@ import pandas as pd
 import numpy as np 
 import math
 import matplotlib.pyplot as plt
+import argparse
 
-shelter_occupancy = pd.read_csv('/Users/soniagrebenisan1/Desktop/personal/dsi_course/topic_2_python/data/Daily shelter overnight occupancy.csv')
-shelter_occupancy.info()
-shelter_occupancy.shape
+# Define command-line arguments
+parser = argparse.ArgumentParser(description='Dataset analysis script')
+parser.add_argument('config', type=str, help='Path to the configuration file')
+parser.add_argument('column1', type=str, help='First column you want to plot')
+parser.add_argument('column2', type=str, help='Second column you want to plot')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Print the path to the configuration file
+print(args.config)
+
+# Read job-specific configuration file 
+config_data = pd.read_csv(args.config)
+
+shelter_occupancy = config_data
+# shelter_occupancy = pd.read_csv('/Users/soniagrebenisan1/Desktop/personal/dsi_course/topic_2_python/data/Daily shelter overnight occupancy.csv')
 
 def lower_names(df):
     return df.lower()
 shelter_occupancy = shelter_occupancy.rename(columns=lower_names)
-shelter_occupancy[['organization_id', 'shelter_id', 'location_id', 'program_id']] = shelter_occupancy[['organization_id', 'shelter_id', 'location_id', 'program_id']].astype('string')
-shelter_occupancy['occupancy_date'] = pd.to_datetime(shelter_occupancy['occupancy_date'])
-shelter_occupancy['funding_utilization'] = shelter_occupancy['capacity_actual_bed'] / shelter_occupancy['capacity_funding_bed']
 
 fig, ax = plt.subplots()
 
 occupied_beds = ax.scatter(shelter_occupancy['occupancy_date']
-            , shelter_occupancy['occupied_beds']
+            # , shelter_occupancy['occupied_beds']  # original
+            , shelter_occupancy[args.column1]
+            # , c=['tab:blue']  # original
             , c=['tab:blue']
             , marker=(5,1) # because it's cute and happy
             )
 unoccupied_beds = ax.scatter(shelter_occupancy['occupancy_date']
-            , shelter_occupancy['unoccupied_beds']
-            , c=['tab:orange']
+            # , shelter_occupancy['unoccupied_beds']  # original
+            , shelter_occupancy[args.column2]
+            # , c=['tab:orange']  # original
+            , c=['tab:orange']  
             , marker=(5,1) # and shelter data is sad
             )
 
